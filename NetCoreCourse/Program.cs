a48f8f55f7e5c18bc99bf7bcd7d8b47881cddc13
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-17 08:26:24 +0800

    Add project files.

---
+++ NetCoreCourse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetCoreCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Amir Ashraf");
+            Console.ReadLine();
+        }
+    }
+}
