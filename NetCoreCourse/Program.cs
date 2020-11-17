ff6a075977c947237af31a4a4f8be95527fceb1a
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-17 22:52:00 +0800

    Learning Variables

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -6,7 +6,14 @@ namespace NetCoreCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string characterName = "John";
+            int characterAge = 35;
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
             Console.ReadLine();
         }
     }
