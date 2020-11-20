071579b4a7e0a07c6143cfc04ddf43a3fc2c824c
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-20 23:10:45 +0800

    Strings

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -2,20 +2,23 @@
 
 namespace NetCoreCourse
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string phrase = "Giraffe Academy";
-            char grade = 'C';
-            int age = 30;
-            double gpa = 3.0;
-            bool isMale = true;
-
-            Console.WriteLine(age);
-
-            Console.ReadLine();  
-
+            Console.WriteLine("Giraffe\nAcademy");
+            Console.WriteLine("Giraffe\"Academy");
+            Console.WriteLine(phrase);
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Contains("academy"));
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase.IndexOf("Academy"));
+            Console.WriteLine(phrase.IndexOf('f'));
+            Console.WriteLine(phrase.Substring(8));
+            Console.WriteLine(phrase.Substring(8, 3));
+            Console.ReadLine();
         }
     }
 }
