1516205a5f8b350817f81e967742593a558078a8
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-21 01:21:31 +0800

    Getting User Input

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -6,19 +6,11 @@ namespace NetCoreCourse
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(50);
-            Console.WriteLine(50 % 8);
-            Console.WriteLine(4 + 2 * 6);
-            Console.WriteLine(5.1 + 8.1);
-            Console.WriteLine(5 / 2);
-            Console.WriteLine(5 / 2.0);
-            int num = 6;
-            num++;
-            Console.WriteLine(num);
-            Console.WriteLine(Math.Pow(3, 2));
-            Console.WriteLine(Math.Sqrt(36));
-            Console.WriteLine(Math.Max(36,40));
-            Console.WriteLine(Math.Round(36.8));
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old. ");
             Console.ReadLine();
         }
     }
