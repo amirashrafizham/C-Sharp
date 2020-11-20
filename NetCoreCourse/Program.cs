c23ebf7122657097117ad96cac16eadb573902b0
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-20 22:35:58 +0800

    Data Types

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -6,15 +6,16 @@ namespace NetCoreCourse
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge = 35;
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("He was " + characterAge + " years old");
-            characterName = "Mike";
-            Console.WriteLine("He really liked the name " + characterName);
-            Console.WriteLine("But didn't like being " + characterAge);
-            Console.WriteLine("Changing the repo from private to public");
-            Console.ReadLine();
+            string phrase = "Giraffe Academy";
+            char grade = 'C';
+            int age = 30;
+            double gpa = 3.0;
+            bool isMale = true;
+
+            Console.WriteLine(age);
+
+            Console.ReadLine();  
+
         }
     }
 }
