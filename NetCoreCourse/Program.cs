0676fe4d1e830b51cb796d1ca82c6fb51b4ee222
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-23 22:42:56 +0800

    Building a Mad Lib

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -6,12 +6,15 @@ namespace NetCoreCourse
     {
         private static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num1 + num2);
-            
+            Console.Write("Pick a color: ");
+            string color = Console.ReadLine();
+            Console.Write("Pick a noun: ");
+            string pluralNoun = Console.ReadLine();
+            Console.Write("Pick a celebrity: ");
+            string celebrity = Console.ReadLine();
+            Console.WriteLine($"Roses are {color}");
+            Console.WriteLine($"{pluralNoun} are blue");
+            Console.WriteLine($"I Love {celebrity}");
         }
     }
 }
