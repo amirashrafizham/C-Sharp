68bfe968d11b555bbf1c04f9bae1c9728ee97fcb
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-11-24 23:26:45 +0800

    While Loops

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -6,50 +6,17 @@ namespace NetCoreCourse
     {
         private static void Main(string[] args)
         {
-            int dayNum;
-            Console.Write("Insert an integer: ");
-            dayNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"It is {GetDay(dayNum)}");
-            Console.ReadLine();
-        }
-
-        private static string GetDay(int dayNum)
-        {
-            string dayName;
-            switch (dayNum)
+            int index = 6;
+            while (index <= 10)
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-
-                case 1:
-                    dayName = "Monday";
-                    break;
-
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-
-                case 4:
-                    dayName = "Thursday";
-                    break;
-
-                case 5:
-                    dayName = "Friday";
-                    break;
-
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid day number";
-                    break;
+                Console.WriteLine(index);
+                index++;
             }
-            return dayName;
+            do
+            {
+                Console.WriteLine("You execute first, then check later");
+            } while (index <= 5);
+            Console.ReadLine();
         }
     }
 }
