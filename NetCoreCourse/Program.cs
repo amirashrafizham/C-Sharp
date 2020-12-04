8f7d431b6539c214a0028f086ff6cf531dce285f
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-12-04 09:20:24 +0800

    Elegant way to do If Statement

--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -7,61 +7,18 @@ namespace NetCoreCourse
         private static void Main(string[] args)
         {
             int counter;
-            int[] arrNumbers = { 4, 8, 15, 16, 23, 42 };
-            string suffix;
-            for (counter = 0; counter < arrNumbers.Length; counter++)
-            {
-                switch (counter)
-                {
-                    case 0:
-                        suffix = "th";
-                        break;
-                    case 1:
-                        suffix = "st";
-                        break;
-                    case 2:
-                        suffix = "nd";
-                        break;
-                    case 3:
-                        suffix = "rd";
-                        break;
-                    default:
-                        suffix = "th";
-                        break;
-                }
+            int numInput;
+            string message;
+            Console.Write("How many times do you want to loop? ");
+            numInput = Convert.ToInt32(Console.ReadLine()); 
+            
+            for (counter = 0; counter<numInput; counter++) {
 
-                Console.WriteLine($"The {counter}{suffix} value of the array is: {arrNumbers[counter]}");
+                message = (counter % 2 == 0) ? "even number" : "odd number";
+                Console.WriteLine($"The loop is currently at {counter} and it is an {message}");
             }
+
             Console.ReadLine();
-        }
-
-        private static void GuessNumber(double count, double end, double actual)
-        {
-            string answer;
-            double guess;
-            while (count <= end)
-            {
-                Console.Write("Guess a number: ");
-                guess = Convert.ToDouble(Console.ReadLine());
-                if (guess == actual)
-                {
-                    answer = "You are correct";
-                    Console.WriteLine(answer);
-                    break;
-                }
-                else
-                {
-                    if (end - count == 0)
-                    {
-                        answer = $"You have run out of tries";
-                        Console.WriteLine(answer);
-                        break;
-                    }
-                    answer = $"Try again. You have {end - count} guesses remaining";
-                    Console.WriteLine(answer);
-                    count++;
-                }
-            }
         }
     }
 }
---
+++ Revision_and_PlayGround/Revision_and_PlayGround/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Revision_and_PlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many times do you want to loop: ");
+            int numLoop;
+            numLoop = Convert.ToInt32(Console.ReadLine());
+            int Counter;
+            string message;
+            for (Counter = 0; Counter < numLoop; Counter++)
+            {
+                message = Counter % 2 == 0 ? "Even" : "Odd";
+                
+                Console.WriteLine($"Now the loop is at : {Counter}, and the number is {message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
