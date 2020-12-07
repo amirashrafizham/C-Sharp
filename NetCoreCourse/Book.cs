04822cb5fcdf882f811124feb84b3d063084618b
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-12-07 22:46:50 +0800

    Constructors

--- NetCoreCourse/Book.cs
+++ NetCoreCourse/Book.cs
@@ -6,8 +6,17 @@ namespace NetCoreCourse
 {
     class Book
     {
+
         public string title;
         public string author;
         public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+            
+        }
     }
 }
--- NetCoreCourse/Program.cs
+++ NetCoreCourse/Program.cs
@@ -7,15 +7,10 @@ namespace NetCoreCourse
         private static void Main(string[] args)
 
         {
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "J.K. Rowling";
-            book1.pages = 400;
+            Book book1 = new Book("Harry Potter","J.K. Rowling",400);
 
-            Book book2 = new Book();
-            book2.title = "Lord of the Rings";
-            book2.author = "Tolkien";
-            book2.pages = 700;
+            Book book2 = new Book("Lord of the Rings","Tolkien",700);
+
             Console.WriteLine($"The author of {book1.title} is {book1.author} and it has {book1.pages} pages" );
             Console.WriteLine($"The author of {book2.title} is {book2.author} and it has {book2.pages} pages" );
 
