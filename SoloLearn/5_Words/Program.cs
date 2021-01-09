94090259db6a95ec82bfc2c0ad9cec382af11c4f
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-01-09 23:20:31 +0800

    Exercise 5

---
+++ SoloLearn/5_Words/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _5_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int x = 0;
+            //your code goes here
+
+            foreach (var item in words)
+            {
+                if (item.Contains(letter))
+                {
+                    Console.WriteLine(item);
+                }
+                else
+                {
+
+                    x = x + 1;
+
+                }
+            }
+            if (x == words.Length)
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}
