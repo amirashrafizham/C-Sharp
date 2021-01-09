820ca40b95d83b47fb0e8f9ef27ed247978525bf
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-01-09 16:09:37 +0800

    Committing for SoloLearn

--- Revision_and_PlayGround/Revision_and_PlayGround/Program.cs
+++ Revision_and_PlayGround/Revision_and_PlayGround/Program.cs
@@ -2,48 +2,19 @@
 
 namespace Revision_and_PlayGround
 {
-    internal class Program
+    class Program
     {
         private static void Main(string[] args)
+
         {
-            int actualNumber;
-            int guessNumber;
-            int loopNumber = 3;
-            int min = 0;
-            int max = 5;
-            int counter;
-            Random rd = new Random();
-            actualNumber = rd.Next(min, max);
-            Console.WriteLine("The actual number is " + actualNumber);
-
-            for (counter = 0; counter < loopNumber; counter++)
-            {
-                Console.Write("Pick a number between 1-5: ");
-                guessNumber = Convert.ToInt32(Console.ReadLine());
-
-                if (guessNumber == actualNumber)
-                {
-                    Console.WriteLine("Correct!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Wrong!");
-                }
-            }
+            Console.Write("What is the name of your band? ");
+            string bandName = Console.ReadLine();
+            Console.Write("How many people are in the band?");
+            int bandMember = Console.ReadLine();
+            Console.WriteLine("{bandName} has {bandMember} members.");
 
             Console.ReadLine();
-
-            // Console.Write($"{numberGuessing(guessNumber, actualNumber)}. The actual number is {actualNumber}  ");
         }
-
-        /*
-                private static string numberGuessing(int guessNumber, int actualNumber)
-                {
-                    string message;
-                    message = (guessNumber == actualNumber) ? "Equals" : "Not Equals";
-                    return message;
-                }
-        */
+        
     }
 }
---
+++ SoloLearn/1_AreaCircle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1_AreaCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial_NoRecursion(6);
+
+            int rec_Factorial = Factorial_Recursion(6);
+            Console.WriteLine(rec_Factorial);
+            Console.ReadLine();
+
+        }
+
+        static void Factorial_NoRecursion(int n)
+        {
+            int x = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                x = x * i;
+            }
+            Console.WriteLine($"The factorial of {n} is {x}");
+        }
+
+        static int Factorial_Recursion(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial_Recursion(n - 1);
+        }
+
+    }
+}
