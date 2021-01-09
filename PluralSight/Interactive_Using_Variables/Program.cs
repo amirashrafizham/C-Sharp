c5f57383d94699afbded41f2cb900028d6707eae
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-01-09 16:08:07 +0800

    First Time committing for Pluralsight Interactive Course

---
+++ PluralSight/Interactive_Using_Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Interactive_Using_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the name of your band? ");
+            string nameBand = Console.ReadLine();
+            Console.Write("How many people are in your band? ");
+            int memberBand = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{nameBand} has {memberBand} members");
+            Console.ReadLine();
+        }
+    }
+}
