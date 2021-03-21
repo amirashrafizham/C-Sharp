3e224924aa56940436e806a9a740ac2345f44d29
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-03-21 13:28:29 +0800

    Reference Fundamentals

---
+++ CodeAcademy/References/Reference_Fundamentals/Book.cs
@@ -0,0 +1,22 @@
+namespace Reference_Fundamentals
+{
+    class Book
+    {
+        public string Author
+        { get; private set; }
+
+        public string Title
+        { get; private set; }
+
+        public Book(string author = "Unknown", string title = "Untitled")
+        {
+            Author = author;
+            Title = title;
+        }
+
+        public virtual string Stringify()
+        {
+            return "This is a Book object!";
+        }
+    }
+}
---
+++ CodeAcademy/References/Reference_Fundamentals/IFlippable.cs
@@ -0,0 +1,8 @@
+namespace Reference_Fundamentals
+{
+    interface IFlippable
+    {
+        int CurrentPage { get; set; }
+        void Flip();
+    }
+}
