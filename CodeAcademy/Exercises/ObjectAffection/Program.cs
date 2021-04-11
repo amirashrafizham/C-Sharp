b27afde6be53ab4527f5e52746ecade082b99ac7
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-04-11 15:42:01 +0800

    Added string array initialization for Object Affection exercise

--- CodeAcademy/Exercises/ObjectAffection/Program.cs
+++ CodeAcademy/Exercises/ObjectAffection/Program.cs
@@ -8,6 +8,8 @@ namespace CodeCademy_Exercise
         {
             Profile sam = new Profile("Sam Drakkila", 18, "New York", "USA", "he/him");
             Console.WriteLine(sam.Validate());
+            sam.SetHobbies(new string[] { "playing guitar", "jogging", "playing guitar" }); //NEW is needed to INITIALIZE the string array. Wihout it, it doesn't consider it as an array
+            Console.WriteLine(sam.Validate());
 
         }
     }
