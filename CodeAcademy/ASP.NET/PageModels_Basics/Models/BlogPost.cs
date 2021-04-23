d8271c89f35eddb740ccbd32d2f3e9ea2e26db5b
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-04-23 16:41:36 +0800

    PageModels exercise

---
+++ CodeAcademy/ASP.NET/PageModels_Basics/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+using System;
+namespace PageModels.Models
+{
+    public class BlogPost
+    {
+        public string Title
+        { get; set; }
+
+        public DateTime Date
+        { get; set; }
+
+        public string Body
+        { get; set; }
+
+        public BlogPost(string title, DateTime date, string body)
+        {
+            Title = title;
+            Date = date;
+            Body = body;
+        }
+    }
+}
---
+++ CodeAcademy/ASP.NET/PageModels_Basics/Pages/Index.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace PageModels.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string RequestMethod
+        { get; set; }
+
+        public string RequestValues
+        { get; set; }
+
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public DateTime Date { get; set; }
+        [BindProperty]
+        public string Body { get; set; }
+
+        public void OnGet()
+        {
+            // For debugging
+            RequestMethod = "GET";
+            RequestValues = "n/a";
+
+            // Assign property values here
+            Title = "Cuban Midnight Sandwich";
+            Date = new DateTime(2000, 3, 24);
+            Body = "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
+        }
+
+        public void OnPost()
+        {
+            // For debugging
+            RequestMethod = "POST";
+            RequestValues = GetFormValues();
+
+            // Assign property values here
+        }
+
+        // For debugging
+        private string GetFormValues(bool ignoreRequestVerificationToken = true)
+        {
+            string formData = "";
+            string separator = " | ";
+
+            foreach (var pair in this.Request.Form)
+            {
+                if (ignoreRequestVerificationToken && pair.Key == "__RequestVerificationToken")
+                {
+                    continue;
+                }
+                else
+                {
+                    formData += pair.Key + ": " + Request.Form[pair.Key] + separator;
+                }
+            }
+
+            if (formData.EndsWith(separator))
+            {
+                formData = formData.Substring(0, formData.Length - separator.Length);
+            }
+
+            return formData;
+        }
+    }
+}
